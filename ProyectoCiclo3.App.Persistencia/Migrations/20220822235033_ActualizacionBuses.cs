66c1b954e1339b5ab348b6c61c746795537481bc
pforerol <pforerol@gmail.com>
2022-08-29 19:41:18 -0500

    FirtsCommit

---
+++ ProyectoCiclo3.App.Persistencia/Migrations/20220822235033_ActualizacionBuses.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoCiclo3.App.Persistencia.Migrations
+{
+    public partial class ActualizacionBuses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Buses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    modelo = table.Column<int>(type: "int", nullable: false),
+                    kilometraje = table.Column<int>(type: "int", nullable: false),
+                    num_asientos = table.Column<int>(type: "int", nullable: false),
+                    placa = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buses", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Buses");
+        }
+    }
+}
